4e2f0291eb5434351388e95ef1ba24c936cac68f
juexinwang <juexinwang@hotmail.com>
2019-10-24 19:35:52 -0500

    preprocessing

--- Preprocessing_scNetwork.py
+++ Preprocessing_scNetwork.py
@@ -18,60 +18,18 @@ parser.add_argument('--expression-name', type=str, default='TGFb', #1
 
 args = parser.parse_args()
 
-# Load gene expression into sparse matrix
-def read_feature_file_sparse(filename, geneList):
-    samplelist=[]
-    featurelist=[]
-    data =[]
-    selectDict=[]
-    selectList=[]
-    count = -1
-    with open(filename) as f:
-        lines = f.readlines()
-        for line in lines:            
-            line = line.strip()
-            words = line.split()
-            if count == -1:
-                tcount =0
-                for word in words:
-                    selectDict[word] = tcount
-                    tcount = tcount + 1
-                for gene in geneList:
-                    if gene in selectDict:
-                        selectList.append(selectDict[gene])
-                    else:
-                        print(str(gene)+' is not in the input')
-            if count >= 0:
-                tmplist =[]
-                for word in words:
-                    tmplist.append(word)
-                
-                data_count = 0
-                for item in selectList:
-                    featurelist.append(count)
-                    samplelist.append(data_count)
-                    data.append(float(tmplist[item]))
-                    data_count += 1
-            count += 1
-    f.close()
-    feature = scipy.sparse.csr_matrix((data, (samplelist,featurelist)), shape=(len(samplelist),len(geneList)))   
-    return feature
+# Read network and expression
+# output geneList, geneDict
+def preprocess_network(edge_filename, feature_filename):
 
-# Load gold standard edges into sparse matrix
-# No edge types
-# output mtx, geneList, geneDict, tfDict
-def read_edge_file_csc(filename):
-    row=[]
-    col=[]
-    data=[]
-    # geneList, geneDict, tfDict
+    # geneList, geneDict
     geneList=[]
+    tList=[]
+    tgeneDict={}
     geneDict={}
-    tfDict={}
 
-    count = 0
     genecount = 0
-    with open(filename) as f:
+    with open(edge_filename) as f:
         lines = f.readlines()
         for line in lines:
             line = line.strip()
@@ -79,133 +37,212 @@ def read_edge_file_csc(filename):
             end1 = words[0]
             end2 = words[1]
 
-            #Add to sparse matrix
-            row.append(end1)
-            col.append(end2)
-            data.append(1.0)
-            row.append(end2)
-            col.append(end1)
-            data.append(1.0)
-
             # geneList, geneDict, tfDict
-            if end1 not in geneDict:
-                geneDict[end1] = genecount
-                geneList.append(end1)
+            if end1 not in tgeneDict:
+                tgeneDict[end1] = genecount
+                tList.append(end1)
                 genecount = genecount + 1
-            if end2 not in geneDict:
-                geneDict[end2] = genecount
-                geneList.append(end2)
+            if end2 not in tgeneDict:
+                tgeneDict[end2] = genecount
+                tList.append(end2)
                 genecount = genecount + 1
-            if end1 not in tfDict:
-                tfDict[end1] = ''
-            count += 1
+            # if end1 not in tfDict:
+            #     tfDict[end1] = tgeneDict[end1]
+    f.close()
+
+    count = 0
+    exDict={}
+    with open(feature_filename) as f:
+        lines = f.readlines()
+        for line in lines:            
+            line = line.strip()
+            words = line.split(',')
+            if count == 0:
+                tcount =0
+                for word in words:
+                    exDict[word] = tcount
+                    tcount = tcount + 1
+            else:
+                break
+            count = count+1
+    f.close()
+
+    count = 0
+    for gene in tList:
+        if gene in exDict:
+            geneList.append(gene)
+            geneDict[gene] = count
+            count +=1
+
+    return geneList, geneDict
+
+# Load gold standard edges into sparse matrix
+# No edge types
+# output mtx, tfDict
+def read_edge_file_csc(filename, geneDict):
+    row=[]
+    col=[]
+    data=[]
+
+    tfDict={}
+    count = 0
+    with open(filename) as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.strip()
+            words = line.split()
+            end1 = words[0]
+            end2 = words[1]
+            
+            #Add to sparse matrix
+            if end1 in geneDict and end2 in geneDict:
+                row.append(geneDict[end1])
+                col.append(geneDict[end2])
+                data.append(1.0)
+                row.append(geneDict[end2])
+                col.append(geneDict[end1])
+                data.append(1.0)
+                count += 1
+                if end1 not in tfDict:
+                    tfDict[end1] = geneDict[end1]
     f.close()
     row = np.asarray(row)
     col = np.asarray(col)
     data = np.asarray(data)
     #check and get full matrix
-    mtx = scipy.sparse.csc_matrix((data, (row, col)), shape=(len(geneList), len(geneList)))
-    return mtx, geneList, geneDict, tfDict
+    mtx = scipy.sparse.csc_matrix((data, (row, col)), shape=(len(geneDict), len(geneDict)))
+    return mtx, tfDict
 
-# calculate Pearson's Correlation coefficient of gene expression
-def pearsonMatrix(data, threshold=0.8):
-    row=[]
-    col=[]
-    edata=[]
-    for i in np.arange(data.shape[1]):
-        for j in np.arange(data.shape[1]):
-            corr, _ = pearsonr(data[:,i], data[:,j])
-            if abs(corr) >= threshold:
-                row.append(i)
-                col.append(j)
-                edata.append(1.0)
-    
-    row = np.asarray(row)
-    col = np.asarray(col)
-    edata = np.asarray(edata)
+# Load gene expression into sparse matrix
+def read_feature_file_sparse(filename, geneList, geneDict):
+    samplelist=[]
+    featurelist=[]
+    data =[]
+    selectDict={}
+    selectList=[]
+    count = -1
+    with open(filename) as f:
+        lines = f.readlines()
+        for line in lines:            
+            line = line.strip()
+            words = line.split(',')
+            if count == -1:
+                tcount =0
+                for word in words:
+                    if word in geneDict:
+                        selectDict[word] = tcount
+                    tcount = tcount + 1
+                ntcount = 0
+                ytcount = 0
+                for gene in geneList:
+                    if gene in selectDict:
+                        selectList.append(selectDict[gene])
+                        ytcount += 1
+                    else:
+                        print(str(gene)+' is not in the input')
+                        ntcount += 1
+                print(str(ytcount)+"\t"+str(ntcount))
+            if count >= 0:
+                tmplist =[]
+                for word in words:
+                    tmplist.append(float(word))
+                avgtmp = np.sum(tmplist)/float(len(tmplist))
+                
+                data_count = 0
+                for item in selectList:
+                    samplelist.append(count)
+                    featurelist.append(data_count)
+                    # data.append(float(tmplist[item]))
+                    if tmplist[item]>=avgtmp:
+                        data.append(1)
+                    else:
+                        data.append(0)
+                    data_count += 1
+            count += 1
+    f.close()
+    # As dream: rows as genes, columns as samples: This is transpose of the original scRNA data
+    feature = scipy.sparse.csr_matrix((data, (featurelist, samplelist)), shape=(len(selectList),count))   
+    return feature
+
+
+
+def read_edge_file_dict(filename, geneDict):
+    graphdict={}
+    tdict={}
+    count = 0
+    with open(filename) as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.strip()
+            words = line.split()
+            end1 = words[0]
+            end2 = words[1]
+            if end1 in geneDict and end2 in geneDict:
+                tdict[geneDict[end1]]=""
+                tdict[geneDict[end2]]=""
+                if geneDict[end1] in graphdict:
+                    tmplist = graphdict[geneDict[end1]]
+                else:
+                    tmplist = []
+                tmplist.append(geneDict[end2])
+                graphdict[geneDict[end1]]= tmplist
+            count += 1
+    f.close()
     #check and get full matrix
-    mtx = scipy.sparse.csc_matrix((edata, (row, col)), shape=(data.shape[1], data.shape[1]))
-    return mtx
-
-# calculate Mutual Information of gene expression 
-def mutualMatrix(data, thresholdfold=3, bin=100):
-    row=[]
-    col=[]
-    edata=[]
-    total = []
-    for i in np.arange(data.shape[1]):
-        for j in np.arange(data.shape[1]):
-            total.append(calc_MI(data[:,i], data[:,j], bin))
-    
-    total = np.asarray(total)
-    threshold = np.mean(total)+thresholdfold*np.std(total)
-
-    for i in np.arange(data.shape[1]):
-        for j in np.arange(data.shape[1]):
-            if calc_MI(data[:,i], data[:,j], bin) >= threshold:
-                row.append(i)
-                col.append(j)
-                edata.append(1.0)
-    
-    row = np.asarray(row)
-    col = np.asarray(col)
-    edata = np.asarray(edata)
-    #check and get full matrix
-    mtx = scipy.sparse.csc_matrix((edata, (row, col)), shape=(data.shape[1], data.shape[1]))
-    return mtx
-
-def calc_MI(x, y, bins):
-    c_xy = np.histogram2d(x, y, bins)[0]
-    mi = mutual_info_score(None, None, contingency=c_xy)
-    return mi
-
-# calculate random
-def randomMatrix(data, threshold=0.003):
-    row=[]
-    col=[]
-    edata=[]
-    for i in np.arange(data.shape[1]):
-        for j in np.arange(data.shape[1]):
-            if np.random.random_sample() <= threshold:
-                row.append(i)
-                col.append(j)
-                edata.append(1.0)
-    
-    row = np.asarray(row)
-    col = np.asarray(col)
-    edata = np.asarray(edata)
-    #check and get full matrix
-    mtx = scipy.sparse.csc_matrix((edata, (row, col)), shape=(data.shape[1], data.shape[1]))
-    return mtx
-
-# Setting of Dream dataset size
-rowDict={}
-colDict={}
-rowDict['1']=805
-rowDict['3']=805
-rowDict['4']=536
-colDict['1']=1643
-colDict['3']=4511
-colDict['4']=5950
-
+    for i in range(len(geneDict)):
+        if i not in tdict:
+            graphdict[i]=[]
+    return graphdict
 
 networkname=args.network_name
-expressionname=arg.expression_name
+expressionname=args.expression_name
 
 if args.network_name=='ttrust':
-    networkname = 'ttrust_rawdata.human.tsv'
+    networkname = 'trrust_rawdata.human.tsv'
 
 if args.expression_name=='TGFb':
-    expressionname = 'HMLE_TGFb_day_8_10.csv'
+    # expressionname = 'HMLE_TGFb_day_8_10.csv'
+    expressionname = 'HMLE_TGFb_day_8_10_part.csv'
 elif args.expression_name=='test':
     expressionname = 'test_data.csv'
 
-edge_filename    = "data/"+networkname
-feature_filename = "data/"+expressionname
+edge_filename    = "/home/wangjue/biodata/scData/network/"+networkname
+feature_filename = "/home/wangjue/biodata/scData/"+expressionname
 
-graphcsc, geneList, geneDict, tfDict = read_edge_file_csc(edge_filename)
-allx = read_feature_file_sparse(feature_filename, geneList)
+geneList, geneDict = preprocess_network(edge_filename, feature_filename)
 
-pickle.dump(allx, open( "ind.dream"+expressionname+".allx", "wb" ) )
-pickle.dump(graphcsc, open( "ind.dream"+expressionname+".csc", "wb" ) )
+graphcsc, tfDict = read_edge_file_csc(edge_filename, geneDict)
+feature = read_feature_file_sparse(feature_filename, geneList, geneDict)
+graphdict = read_edge_file_dict(edge_filename, geneDict)
+
+x = feature[0:100]
+tx = feature[0:100]
+allx = feature[100:]
+# allx = feature
+testindex = ""
+for i in range(100):
+    testindex = testindex + str(i) + "\n"
+
+
+with open("data/sc/gene.txt",'w') as fw:
+    count = 0
+    for item in geneList:
+        fw.write(str(count)+"\t"+item+"\n")
+        count += 1
+
+with open("data/sc/TF.txt",'w') as fw:
+    count = 0
+    for key in tfDict:
+        fw.write(key+"\t"+str(tfDict[key])+"\n")
+        count += 1
+
+pickle.dump(allx, open( "data/sc/ind."+args.expression_name+".allx", "wb" ) )
+pickle.dump(graphcsc, open( "data/sc/ind."+args.expression_name+".csc", "wb" ) )
+
+pickle.dump(x, open( "data/sc/ind."+args.expression_name+".x", "wb" ) )
+pickle.dump(tx, open( "data/sc/ind."+args.expression_name+".tx", "wb" ) )
+pickle.dump(graphdict, open( "data/sc/ind."+args.expression_name+".graph", "wb" ) )
+with open ("data/sc/ind."+args.expression_name+".test.index", 'w') as fw:
+    fw.writelines(testindex)
+    fw.close()
 
