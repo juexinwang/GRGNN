7e94e9708606661fcebe06ba62c0748f5365a92e
juexinwang <juexinwang@hotmail.com>
2019-11-06 23:21:27 -0600

    test

--- Preprocessing_scNetwork.py
+++ Preprocessing_scNetwork.py
@@ -5,6 +5,7 @@ from copy import deepcopy
 from scipy import interpolate
 from sklearn.metrics import mutual_info_score
 from scipy.stats import pearsonr
+from scipy.spatial import distance_matrix
 import scipy.sparse
 import sys
 import pickle
@@ -122,6 +123,58 @@ def read_edge_file_csc(filename, geneDict):
     #Output for matlab
     return mtx, tfDict, row, col, data
 
+class KNNEdge:
+    def __init__(self,row,col):
+        self.row=row
+        self.col=col
+
+# Not use it now
+# Calculate KNN graph, return row and col
+def cal_distanceMatrix(featureMatrix, k=5):
+    distMat = distance_matrix(featureMatrix,featureMatrix)
+    edgeList=[]
+
+    for i in np.arange(distMat.shape[0]):
+        res = distMat[:,i].argsort()[:k]
+        for j in np.arange(k):
+            edgeList.append(KNNEdge(i,res[j]))
+    
+    return edgeList
+
+
+# For cell,use feature matrix as input, row as cells, col as genes
+# Load gold standard edges into sparse matrix
+# No edge types
+# output mtx, tfDict
+# Additional outfile for matlab
+def read_edge_file_csc_cell(edgeList, nodesize, k=5):
+    row=[]
+    col=[]
+    data=[]
+
+    
+    for edge in edgeList:
+        row.append(edge.row)
+        col.append(edge.col)
+        data.append(1.0)
+        row.append(edge.col)
+        col.append(edge.row)
+        data.append(1.0)
+
+    row = np.asarray(row)
+    col = np.asarray(col)
+    data = np.asarray(data)
+    #check and get full matrix
+    mtx = scipy.sparse.csc_matrix((data, (row, col)), shape=(nodesize, nodesize))
+    
+    #python output
+    # return mtx, tfDict
+
+    #Output for matlab
+    return mtx, row, col, data
+
+
+
 # Load gene expression into sparse matrix
 def read_feature_file_sparse(filename, geneList, geneDict):
     samplelist=[]
@@ -153,6 +206,7 @@ def read_feature_file_sparse(filename, geneList, geneDict):
                         ntcount += 1
                 print(str(ytcount)+"\t"+str(ntcount))
             if count >= 0:
+                #discrete here
                 tmplist =[]
                 for word in words:
                     tmplist.append(float(word))
@@ -197,6 +251,82 @@ def read_feature_file_sparse(filename, geneList, geneDict):
     return feature, dim2out
 
 
+# For node as cell
+# Load gene expression into sparse matrix
+def read_feature_file_sparse_cell(filename, geneList, geneDict):
+    samplelist=[]
+    featurelist=[]
+    data =[]
+    selectDict={}
+    selectList=[]
+    count = -1
+
+    with open(filename) as f:
+        lines = f.readlines()
+        for line in lines:            
+            line = line.strip()
+            words = line.split(',')
+            if count == -1:
+                tcount =0
+                for word in words:
+                    if word in geneDict:
+                        selectDict[word] = tcount
+                    tcount = tcount + 1
+                ntcount = 0
+                ytcount = 0
+                for gene in geneList:
+                    if gene in selectDict:
+                        selectList.append(selectDict[gene])
+                        ytcount += 1
+                    else:
+                        print(str(gene)+' is not in the input')
+                        ntcount += 1
+                print(str(ytcount)+"\t"+str(ntcount))
+            if count >= 0:
+                #discrete here
+                tmplist =[]
+                for word in words:
+                    tmplist.append(float(word))
+                avgtmp = np.sum(tmplist)/float(len(tmplist))
+                
+                data_count = 0
+                for item in selectList:
+                    samplelist.append(data_count)
+                    featurelist.append(count)
+                    # data.append(float(tmplist[item]))
+                    if tmplist[item]>=avgtmp:
+                        data.append(1)
+                    else:
+                        data.append(0)
+                    data_count += 1
+            count += 1
+    f.close()
+    # As dream: rows as genes, columns as samples: This is transpose of the original scRNA data
+    feature = scipy.sparse.csr_matrix((data, (samplelist, featurelist)), shape=(count,len(selectList)))  
+
+    # For Matlab
+    dim2out = [[0.0] * count for i in range(len(selectList))]
+    count = -1
+    with open(filename) as f:
+        lines = f.readlines()
+        for line in lines:            
+            line = line.strip()
+            words = line.split(',')
+            if count >= 0:
+                tmplist =[]
+                for word in words:
+                    tmplist.append(float(word))
+                avgtmp = np.sum(tmplist)/float(len(tmplist))
+                
+                data_count = 0
+                for item in selectList:
+                    dim2out[data_count][count]=float(tmplist[item])
+                    data_count += 1
+            count += 1
+    f.close()
+
+    return feature, dim2out
+
 
 def read_edge_file_dict(filename, geneDict):
     graphdict={}
@@ -224,6 +354,29 @@ def read_edge_file_dict(filename, geneDict):
     for i in range(len(geneDict)):
         if i not in tdict:
             graphdict[i]=[]
+    return graphdict
+
+def read_edge_file_dict_cell(edgeList, nodesize):
+    graphdict={}
+    tdict={}
+
+    for edge in edgeList:
+        end1 = edge.row
+        end2 = edge.col
+        tdict[end1]=""
+        tdict[end2]=""
+        if end1 in graphdict:
+            tmplist = graphdict[end1]
+        else:
+            tmplist = []
+        tmplist.append(end2)
+        graphdict[end1]= tmplist
+
+    #check and get full matrix
+    for i in range(nodesize):
+        if i not in tdict:
+            graphdict[i]=[]
+
     return graphdict
 
 networkname=args.network_name
@@ -258,10 +411,16 @@ geneList, geneDict = preprocess_network(edge_filename, feature_filename)
 # feature = read_feature_file_sparse(feature_filename, geneList, geneDict)
 
 #python and matlab
-graphcsc, tfDict, rowO, colO, dataO  = read_edge_file_csc(edge_filename, geneDict)
-feature, dim2out = read_feature_file_sparse(feature_filename, geneList, geneDict)
-
-graphdict = read_edge_file_dict(edge_filename, geneDict)
+if args.graph_type=='gene':
+    graphcsc, tfDict, rowO, colO, dataO  = read_edge_file_csc(edge_filename, geneDict)
+    feature, dim2out = read_feature_file_sparse(feature_filename, geneList, geneDict)
+    graphdict = read_edge_file_dict(edge_filename, geneDict)
+elif args.graph_type=='cell':
+    #First generate feature
+    feature, dim2out = read_feature_file_sparse_cell(feature_filename, geneList, geneDict)
+    edgeList = cal_distanceMatrix(feature, k=5)
+    graphcsc, rowO, colO, dataO  = read_edge_file_csc_cell(edgeList, feature.shape[0], k=5)
+    graphdict = read_edge_file_dict_cell(edgeList, feature.shape[0] )
 
 x = feature[0:100]
 tx = feature[0:100]
@@ -271,18 +430,18 @@ testindex = ""
 for i in range(100):
     testindex = testindex + str(i) + "\n"
 
+if args.graph_type=='gene':
+    with open("data/sc/gene.txt",'w') as fw:
+        count = 0
+        for item in geneList:
+            fw.write(str(count)+"\t"+item+"\n")
+            count += 1
 
-with open("data/sc/gene.txt",'w') as fw:
-    count = 0
-    for item in geneList:
-        fw.write(str(count)+"\t"+item+"\n")
-        count += 1
-
-with open("data/sc/TF.txt",'w') as fw:
-    count = 0
-    for key in tfDict:
-        fw.write(key+"\t"+str(tfDict[key])+"\n")
-        count += 1
+    with open("data/sc/TF.txt",'w') as fw:
+        count = 0
+        for key in tfDict:
+            fw.write(key+"\t"+str(tfDict[key])+"\n")
+            count += 1
 
 pickle.dump(allx, open( "data/sc/ind."+args.expression_name+".allx", "wb" ) )
 pickle.dump(graphcsc, open( "data/sc/ind."+args.expression_name+".csc", "wb" ) )
