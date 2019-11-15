9999c9de9317d62de498d9fa8efc7d5af89c0f7b
juexinwang <juexinwang@hotmail.com>
2019-11-15 15:09:12 -0600

    str

--- Preprocessing_DREAM5.py
+++ Preprocessing_DREAM5.py
@@ -192,11 +192,11 @@ with open(feature_filename) as f:
 
 # Calculate Pearson's Correlation coeficient
 pmatrix = pearsonMatrix(data, args.pearson_net)
-np.save('dream'+datasetname+'_pmatrix_'+args.pearson_net+'.npy', pmatrix)
+np.save('dream'+datasetname+'_pmatrix_'+str(args.pearson_net)+'.npy', pmatrix)
 
 # Calculate Mutual Information
 mmatrix = mutualMatrix(data, args.mutual_net)
-np.save('dream'+datasetname+'_mmatrix_'+args.mutual_net+'.npy', mmatrix)
+np.save('dream'+datasetname+'_mmatrix_'+str(args.mutual_net)+'.npy', mmatrix)
 
 # Calculate a random network based on 
 rmatrix = randomMatrix(data, args.random_net)
